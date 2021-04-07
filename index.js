44deecac51696041177eb91e4d7c70ce43f3db19
Henry <hvelas93@gmail.com>
2021-04-07 08:16:49 -0400

    commit tree 8bab6a2a7c04718a285088b1f14025ae98504cc7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+// var customerName = 'bob'
+
+// function upperCaseCustomerName() {
+//     // debugger
+//     var customerName = 'bob'
+//     x = customerName.toUpperCase()
+//     var customerName = x
+//     return customerName
+// }
+
+// upperCaseCustomerName()
+
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'henry'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'steven'
+}
